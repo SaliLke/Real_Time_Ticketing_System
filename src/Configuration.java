bb5cd31170b305ceac1a62bb938b6cf1e2207fb9
SaliLke <sanalilokuge.backup.one@gmail.com>
2024-11-06 22:13:25 +0530

    serilized

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,4 +1,8 @@
 import java.io.Serializable;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Configuration implements Serializable {
     private int totalTickets;
@@ -36,5 +40,26 @@ public class Configuration implements Serializable {
 
     public void setMaxTicketCapacity(int maxTicketCapacity) {
         this.maxTicketCapacity = maxTicketCapacity;
+    }
+
+    public void saveToFile() {
+        String filepath="E:/IIT Stuff/oop/Mine/CW/TicketingSystemCLI";
+        try(FileWriter writer=new FileWriter (filepath)){
+            Gson gson=new Gson ();
+            gson.toJson (this, writer);
+            System.out.println("Saved successfully.");
+        }catch (IOException e) {
+            System.out.println("Failed to save: " + e.getMessage());
+        }
+    }
+    public static Configuration loadFromFile(String filepath) {
+        try(FileReader reader=new FileReader (filepath)){
+            Gson gson=new Gson ();
+            return gson.fromJson (reader, Configuration.class);
+        }catch (IOException e) {
+            System.out.println("Failed to load: " + e.getMessage());
+            //System.out.println("Failed to load configuration, using default settings.");
+            return new Configuration();
+        }
     }
 }
