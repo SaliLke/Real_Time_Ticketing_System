89db36d71fb3c705ecd19f54c5d375664fff6a2c
SaliLke <sanalilokuge.backup.one@gmail.com>
2024-11-06 20:19:47 +0530

    config

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,5 +1,40 @@
 import java.io.Serializable;
 
 public class Configuration implements Serializable {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
 
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+    
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public void setTicketReleaseRate(int ticketReleaseRate) {
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public void setCustomerRetrievalRate(int customerRetrievalRate) {
+        this.customerRetrievalRate = customerRetrievalRate;
+    }
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+
+    public void setMaxTicketCapacity(int maxTicketCapacity) {
+        this.maxTicketCapacity = maxTicketCapacity;
+    }
 }
--- src/TicketingSystemCLI.java
+++ src/TicketingSystemCLI.java
@@ -5,16 +5,26 @@ public class TicketingSystemCLI {
     private static int ticketReleaseRate;
     private static int customerRetrievalRate;
     private static int maxTicketCapacity=10;
+    private static Configuration configuration=new Configuration();
 
     public static void main(String[] args) {
         getInputs ();
+        System.out.println ("Data to be stored in the configuration class:");
+        System.out.println ("Total tickets: " + configuration.getTotalTickets ());
+        System.out.println ("Ticket release rate: " + configuration.getTicketReleaseRate ());
+        System.out.println ("Customer retrieval rate: " + configuration.getCustomerRetrievalRate ());
+        System.out.println ("Max ticket capacity: " + configuration.getMaxTicketCapacity ());
     }
     public static void getInputs(){
         Scanner input = new Scanner(System.in);
         totalTickets= ValidateInputs(input, "Enter the total number of tickets: ",1, maxTicketCapacity);
+        configuration.setTotalTickets(totalTickets);
         ticketReleaseRate= ValidateInputs(input, "Enter the ticket release rate: ",1, Integer.MAX_VALUE);
+        configuration.setTicketReleaseRate (ticketReleaseRate);
         customerRetrievalRate= ValidateInputs(input, "Enter the customer retrieval rate: ",1, Integer.MAX_VALUE);
+        configuration.setCustomerRetrievalRate (customerRetrievalRate);
         maxTicketCapacity= ValidateInputs(input, "Enter the max ticket capacity: ",1, Integer.MAX_VALUE);
+        configuration.setMaxTicketCapacity (maxTicketCapacity);
     }
 
     public static int ValidateInputs(Scanner scanner, String message, int minValue, int maxValue){
